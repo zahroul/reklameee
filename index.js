ba19d4c788549286995b2dc23c22abdc83b91c5b
Zahroul Ulum <moh.zahroul@gmail.com>
2018-01-09 19:38:18 +0700

    Add the styles

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((request, response) => {
-  fs.readFile(`${__dirname}/index.html`, (error, data) => {
-    response.writeHeader(200, { 'Content-Type': 'text/html' });
+function readFile(filePath, fileContentType, response) {
+  fs.readFile(filePath, (error, data) => {
+    response.writeHeader(200, { 'Content-Type': fileContentType });
     response.end(data);
   });
+}
+
+http.createServer((request, response) => {
+  const requestUrl = request.url;
+
+  switch (requestUrl) {
+    case '/app.css':
+      readFile(`${__dirname}${requestUrl}`, 'text/css', response);
+      break;
+    default:
+      readFile(`${__dirname}/index.html`, 'text/html', response);
+  }
 }).listen(process.env.PORT);
