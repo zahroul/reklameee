990c7546e40b7d7d729ad1f12469df4e3da20aa5
Zahroul Ulum <moh.zahroul@gmail.com>
2018-01-13 20:58:31 +0700

    Add the external data getter

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const app = {
+  getBillboardsList() {
+    const request = new XMLHttpRequest();
+
+    request.onreadystatechange = () => {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) JSON.parse(request.response);
+      }
+    };
+
+    request.open('GET', 'reklame-data.json');
+    request.send();
+  },
+};
+
 const billboardItem = document.querySelector('figure');
 
 document.querySelector('button').addEventListener('click', () => {
