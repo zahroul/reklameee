9c4962dba14c66d37a3a49a7d33d6722eed6a383
Zahroul Ulum <moh.zahroul@gmail.com>
2018-01-14 22:26:10 +0700

    Update the external data getter Set it return value as the `Promise` object.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const app = {
   getBillboardsList() {
-    const request = new XMLHttpRequest();
+    return new Promise((resolve) => {
+      const request = new XMLHttpRequest();
 
-    request.onreadystatechange = () => {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) JSON.parse(request.response);
-      }
-    };
+      request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) resolve(JSON.parse(request.response));
+        }
+      };
 
-    request.open('GET', 'reklame-data.json');
-    request.send();
+      request.open('GET', 'reklame-data.json');
+      request.send();
+    });
   },
 };
 
